0a04ec85b0b83df57de71766e731437093bc8121
bbwieland <benbowenwieland@gmail.com>
2022-02-27 12:01:28 -0500

    App update v 1.1

--- DraftVisualization/app.R
+++ DraftVisualization/app.R
@@ -6,12 +6,16 @@ data = read.csv("https://raw.githubusercontent.com/bbwieland/NBADraft/main/draft
 schools = unique(data$College) %>% sort()
 schools = schools[-1]
 
+stats = c("Players","Points","Rebounds","Assists","Games","VORP","WinShares")
+
 comp_plot = function(dataframe,xvar,yvar){
     ggplot(dataframe,aes_string(x = xvar,y = yvar)) + geom_col(fill = "lightgrey",color = "black",alpha = 0.5) + theme_minimal()
 }
 
 ui <- fluidPage(
     h1("NBA Draft Picks Since 1990"),
+    p("Source code for this app can be found on Github: go to https://github.com/bbwieland/NBADraft and navigate to the Draft Visualization folder."),
+    p("Application created by Ben Wieland — follow me on Twitter @BenWieland!"),
     tabsetPanel(type = "tabs",
                 tabPanel("Single-School Stats",sidebarLayout(
                     sidebarPanel(
@@ -30,17 +34,30 @@ ui <- fluidPage(
                         selectInput("schoolcomp1","Select a college:",choices = schools,selected = "Kansas"),
                         selectInput("schoolcomp2","Select a college:",choices = schools,selected = "Duke"),
                         selectInput("comparevariable","Select a stat to compare:",
-                                    choices = c("Players","Points","Rebounds","Assists","Games","VORP","WinShares"),
+                                    choices = stats,
                                     selected = "WinShares")
                     ),
                     mainPanel(
                         tabsetPanel(type = "tabs",
-                                    tabPanel("Visualize",plotOutput("comparisonplot")),
                                     tabPanel("Table",fluidRow(
                                         column(4,tableOutput("school1"))
-                                    ),fluidRow(column(4,tableOutput("school2"))))
+                                    ),fluidRow(column(4,tableOutput("school2")))),
+                                    tabPanel("Visualize",plotOutput("comparisonplot"))
                     )
-                )),)))
+                )),),
+                tabPanel("Overall Leaders",sidebarLayout(
+                    sidebarPanel(
+                        selectInput("sortingstat","Select a stat to sort by:",
+                                    choices = stats,
+                                    selected = "WinShares"),
+                        sliderInput("leadernumber","Number of teams to display:",min = 5,max = 50,value = 20)
+                    ),
+                    mainPanel(
+                        tabsetPanel(type = "tabs",
+                                    tabPanel("Table",fluidRow(column(12,tableOutput("leaders")))),
+                                    tabPanel("Visualize",plotOutput("leadersplot"))
+                    ))
+                ))))
 
 
 
@@ -82,8 +99,30 @@ server <- function(input, output) {
                                      VORP = sum(VORP,na.rm = T),
                                      WinShares = sum(WS,na.rm = T)) %>%
                            mutate(School = input$schoolcomp2)})
+
+    sortedleaders = reactive({data %>% 
+            group_by(College) %>%
+            filter(College != "") %>%
+            summarise(Players = n(),
+                      Games = sum(G,na.rm = T),
+                      Points = sum(TOTPTS,na.rm = T),
+                      Rebounds = sum(TOTTRB,na.rm = T),
+                      Assists = sum(TOTAST,na.rm = T),
+                      VORP = sum(VORP,na.rm = T),
+                      WinShares = sum(WS,na.rm = T)) %>%
+            arrange(desc(!! rlang::sym(c(input$sortingstat)))) %>%
+            mutate(Rank = seq(1:229)) %>%
+            head(n = input$leadernumber)})
     
     schoolcomparison = reactive({rbind(school1(),school2())})
+    
+    compInputX = reactive({"School"})
+    compInputY = reactive(input$comparevariable)
+    
+    output$leaders = renderTable(sortedleaders(),
+                                 striped = T,
+                                 align = "c",
+                                 digits = 1)
     
     output$school1 = renderTable(school1(),
                                  striped = T,
@@ -96,14 +135,28 @@ server <- function(input, output) {
                                  align = "c",
                                  digits = 1)
     
-    compInputX = reactive({"School"})
-    compInputY = reactive(input$comparevariable)
         
     output$comparisonplot = renderPlot({comp_plot(schoolcomparison(),compInputX(),compInputY()) +
             labs(x = "",
                  y = input$comparevariable,
                  title = paste0("Comparing ",input$schoolcomp1," to ",input$schoolcomp2," by NBA ",input$comparevariable))+
-            coord_flip()})
+            
+            coord_flip() +  
+            theme(axis.text=element_text(size=14,hjust = 0.5),
+                  plot.title = element_text(size = 24,hjust = 0.5),
+                  title = element_text(size = 14))})
+    
+    output$leadersplot = renderPlot({
+        ggplot(sortedleaders(),aes(x = reorder(College,!!rlang::sym(c(input$sortingstat))),y = !!rlang::sym(c(input$sortingstat)))) +
+            geom_col() +
+            theme_minimal() +
+            coord_flip() +
+            labs(x = "",
+                 title = paste0("College Leaders in ",input$sortingstat)) +  
+            theme(axis.text=element_text(size=ifelse(input$leadernumber > 30,10,12),hjust = 0.5),
+                  plot.title = element_text(size = 24,hjust = 0.5),
+                  title = element_text(size = 14))
+    })
                             
     
     output$data = renderTable(schooldata(),
